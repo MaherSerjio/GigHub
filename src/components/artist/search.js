4a820fb2bfdf594d6f571627afbc0d51663850d7
Maher Srouji <Maher.Srouji1@gmail.com>
2020-05-09 00:43:51 +0300

    Styling Search

--- src/components/artist/search.js
+++ src/components/artist/search.js
@@ -1,12 +1,22 @@
 import React from "react";
 
 import './search.css';
-
+import '../styles/media.css';
 
 const Search = () => {
     return (
-        <div>
-            <h1>Search Page</h1>
+        <div> 
+               <div className="container">      
+                    <div className="input-group " id="search--input">
+                          <input className="form-control py-2 border-right-0 border" 
+                          type="search" placeholder="Search for an artist…" ></input>
+                            <span className="input-group-append">
+                              <button className="btn btn-outline-secondary border-left-0 border" type="button">
+                                <i className="fa fa-search"></i>
+                              </button>
+                            </span>   
+                    </div>           
+               </div>
         </div>
     );
 };
