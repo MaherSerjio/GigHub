0b7f63930f38acd90a0da39b4b706eeee9d8325c
Maher Srouji <Maher.Srouji1@gmail.com>
2020-05-09 00:32:38 +0300

    Added Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Link} from "react-router-dom";
 
+import Header from './Header';
 import Login from './login';
 import Search from './artist/search';
 import Browse from './artist/browse';
@@ -8,6 +9,7 @@ import Browse from './artist/browse';
 const App = () => {
     return (
         <div>
+            <Header/>
             <BrowserRouter>
             <div>
             <Route path="/" exact component={Login}/>
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a className="navbar-brand" href="/">Shopify Artist Search</a>
+  </nav>
+  );
+};
+
+export default Header;
